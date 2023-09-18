c0b51fc7db44b60238fc00222ddeecd852ade661
Mario Herrera <mherreraalmira@gmail.com>
2023-09-17 19:17:58 -0600

    modify mainLogistic.py to watch how weights evolve

--- Practice 1/mainLogistic.py
+++ Practice 1/mainLogistic.py
@@ -2,16 +2,24 @@ from random import random
 from random import seed
 import matplotlib.pyplot as plt
 import numpy as np
+import plotly.graph_objects as go
 
 
 def error(x_error, y_error):
-    #h_error = w0 + w1 * x_error + w2 * x_error + w3 * (x_error * y_error) + w4 * (x_error ** 2) + w5 * (x_error ** 2)
-    h_error = w0 + w1 * G1 + w2 * G2 + w3 * G3 + w4 * G4
+    # h_error = w0 + w1 * x_error + w2 * x_error + w3 * (x_error * y_error) + w4 * (x_error ** 2) + w5 * (x_error ** 2)
+    h_error = (w0 + w1 * gaussian(x_error, y_error, -5, -5, sigma) +
+               w2 * gaussian(x_error, y_error, -5, 5, sigma) +
+               w3 * gaussian(x_error, y_error, 5, -5, sigma) +
+               w4 * gaussian(x_error, y_error, 5, 5, sigma))
     g_error = 1 / (1 + np.power(np.exp(1), (-1 * h_error)))
     if g_error >= 0.5:
         return g_error, "Red"
     else:
         return g_error, "Blue"
+
+
+def gaussian(x_val, y_val, miu_x, miu_y, sigma_error):
+    return np.exp(-((x_val - miu_x) ** 2 / (2 * sigma_error ** 2) + (y_val - miu_y) ** 2 / (2 * sigma_error ** 2)))
 
 
 if __name__ == '__main__':
@@ -42,7 +50,7 @@ if __name__ == '__main__':
             x2.append(X[i])
             y2.append(Y[i])
 
-    alpha = 0.0009
+    alpha = 0.05
     iterations = 100
 
     w0 = random() - 0.5
@@ -61,66 +69,76 @@ if __name__ == '__main__':
     X, Y = np.meshgrid(x, y)
 
     sigma = 2
-    G1 = np.exp(-((X - (-5)) ** 2 / (2 * sigma ** 2) + (Y - (-5)) ** 2 / (2 * sigma ** 2)))
-    G2 = np.exp(-((X - (-5)) ** 2 / (2 * sigma ** 2) + (Y - 5) ** 2 / (2 * sigma ** 2)))
-    G3 = np.exp(-((X - 5) ** 2 / (2 * sigma ** 2) + (Y - (-5)) ** 2 / (2 * sigma ** 2)))
-    G4 = np.exp(-((X - 5) ** 2 / (2 * sigma ** 2) + (Y - 5) ** 2 / (2 * sigma ** 2)))
+    # G1 = np.exp(-((X - (-5)) ** 2 / (2 * sigma ** 2) + (Y - (-5)) ** 2 / (2 * sigma ** 2)))
+    # G2 = np.exp(-((X - (-5)) ** 2 / (2 * sigma ** 2) + (Y - 5) ** 2 / (2 * sigma ** 2)))
+    # G3 = np.exp(-((X - 5) ** 2 / (2 * sigma ** 2) + (Y - (-5)) ** 2 / (2 * sigma ** 2)))
+    # G4 = np.exp(-((X - 5) ** 2 / (2 * sigma ** 2) + (Y - 5) ** 2 / (2 * sigma ** 2)))
 
-    F = w0 + w1 * G1 + w2 * G2 + w3 * G3 + w4 * G4
-    plt.contour(X, Y, F, [0])
-    # Plotting all
-    plt.show()
+    G1 = gaussian(X, Y, -5, -5, sigma)
+    G2 = gaussian(X, Y, -5, 5, sigma)
+    G3 = gaussian(X, Y, 5, -5, sigma)
+    G4 = gaussian(X, Y, 5, 5, sigma)
+
+    F = 0.5 + w1 * G1 + w2 * G2 + w3 * G3 + w4 * G4
+
+    z1 = np.full(len(x1), 0)
+    z2 = np.full(len(x2), 1)
+    z = np.concatenate((z1, z2))
+
+    trace1 = go.Scatter3d(x=x1 + x2, y=y1 + y2, z=z, mode='markers', marker=dict(size=5, color=z))
+    trace2 = go.Surface(z=F, x=X, y=Y, colorscale='Viridis', showscale=False, opacity=0.5)
+
+    fig = go.Figure(data=[trace1, trace2])
+    fig.show()
 
     train_error_list = []
     for ite in range(iterations):
         # Positive examples
         for i in range(len(x1)):
-            H = w0 + w1 * G1 + w2 * G2 + w3 * G3 + w4 * G4
+            H = (w0 + w1 * gaussian(x1[i], y1[i], -5, -5, sigma) +
+                 w2 * gaussian(x1[i], y1[i], -5, 5, sigma) +
+                 w3 * gaussian(x1[i], y1[i], 5, -5, sigma) +
+                 w4 * gaussian(x1[i], y1[i], 5, 5, sigma))
             G = 1 / (1 + np.power(np.exp(1), (-1 * H)))
             w0 = w0 + alpha * (1 - G)
-            w1 = w1 + alpha * (1 - G) * G1
-            w2 = w2 + alpha * (1 - G) * G2
-            w3 = w3 + alpha * (1 - G) * G3
-            w4 = w4 + alpha * (1 - G) * G4
+            w1 = w1 + alpha * (1 - G) * gaussian(x1[i], y1[i], -5, -5, sigma)
+            w2 = w2 + alpha * (1 - G) * gaussian(x1[i], y1[i], -5, 5, sigma)
+            w3 = w3 + alpha * (1 - G) * gaussian(x1[i], y1[i], 5, -5, sigma)
+            w4 = w4 + alpha * (1 - G) * gaussian(x1[i], y1[i], 5, 5, sigma)
         # Negative examples
         for i in range(len(x2)):
-            H = w0 + w1 * G1 + w2 * G2 + w3 * G3 + w4 * G4
+            H = (w0 + w1 * gaussian(x2[i], y2[i], -5, -5, sigma) +
+                 w2 * gaussian(x2[i], y2[i], -5, 5, sigma) +
+                 w3 * gaussian(x2[i], y2[i], 5, -5, sigma) +
+                 w4 * gaussian(x2[i], y2[i], 5, 5, sigma))
             G = 1 / (1 + np.power(np.exp(1), (-1 * H)))
-            w0 = w0 + alpha * (0 - G)
-            w1 = w1 + alpha * (0 - G) * G1
-            w2 = w2 + alpha * (0 - G) * G2
-            w3 = w3 + alpha * (0 - G) * G3
-            w4 = w4 + alpha * (0 - G) * G4
+            w0 = w0 + alpha * (1 - G)
+            w1 = w1 + alpha * (1 - G) * gaussian(x2[i], y2[i], -5, -5, sigma)
+            w2 = w2 + alpha * (1 - G) * gaussian(x2[i], y2[i], -5, 5, sigma)
+            w3 = w3 + alpha * (1 - G) * gaussian(x2[i], y2[i], 5, -5, sigma)
+            w4 = w4 + alpha * (1 - G) * gaussian(x2[i], y2[i], 5, 5, sigma)
 
         train_error = 0
 
         # Calculating error of the current model with POSITIVE examples
         for i in range(len(x1)):
             g, tag = error(x1[i], y1[i])
-            error = (g - 1) ** 2
-            train_error += error
+            error_value = (g - 1) ** 2
+            train_error += error_value
 
         # Calculating error of the current model with NEGATIVE examples
         for i in range(len(x2)):
             g, tag = error(x2[i], y2[i])
-            error = (g - 0) ** 2
-            train_error += error
+            error_value = (g - 0) ** 2
+            train_error += error_value
         train_error = np.sqrt(train_error)
         train_error_list.append(train_error)
 
-        # Plotting points
-        plt.plot(x1, y1, 'ro')
-        plt.plot(x2, y2, 'bo')
-        # Plotting model
-        x = np.linspace(-10.0, 10.0, 100)
-        y = np.linspace(-10.0, 10.0, 100)
-        X, Y = np.meshgrid(x, y)
-        F = w0 + w1 * G1 + w2 * G2 + w3 * G3 + w4 * G4
-        # F = w0 + w1*G1(X,Y) + w2*G2(X,Y) + w3*G3(X,Y) +w4*G4(X,Y)
-        plt.contour(X, Y, F, [0])
-        # Plotting all
-        plt.show()
+        if ite % 10 == 0:
+            F = w0 + w1 * G1 + w2 * G2 + w3 * G3 + w4 * G4
+            trace2 = go.Surface(z=F, x=X, y=Y, colorscale='Viridis', showscale=False, opacity=0.9)
+            fig = go.Figure(data=[trace1, trace2])
+            fig.show()
 
     print(w0, w1, w2, w3, w4)
-
     plt.plot(train_error_list)
