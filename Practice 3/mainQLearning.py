94aaaf835f10bf4334811bca4049011af254bdf6
MarioDev <mherreraalmira@gmail.com>
2023-09-26 10:56:38 -0600

    fix bug in movement

--- Practice 3/mainQLearning.py
+++ Practice 3/mainQLearning.py
@@ -1,5 +1,6 @@
 import numpy as np
 import keyboard
+import os
 
 
 def clear_previous_move(x, y, ambient_matrix):
@@ -14,6 +15,7 @@ def move(x, y, ambient_matrix):
 if __name__ == '__main__':
 
     ambient = np.zeros((4, 12))
+    ambient[0, 0] = 1
     print(ambient)
     pos_x = 0
     pos_y = 0
@@ -25,29 +27,33 @@ if __name__ == '__main__':
             print("Escape key pressed. Exiting Loop!!")
             break
         elif keyboard.is_pressed('up') and not action:
-            print("Moving down!")
-            clear_previous_move(pos_x, pos_y, ambient)
-            pos_x -= 1
-            move(pos_x, pos_y, ambient)
-            action = True
+            if pos_x - 1 >= 0:
+                print("Moving down!")
+                clear_previous_move(pos_x, pos_y, ambient)
+                pos_x -= 1
+                move(pos_x, pos_y, ambient)
+                action = True
         elif keyboard.is_pressed('down') and not action:
-            print("Moving down!")
-            clear_previous_move(pos_x, pos_y, ambient)
-            pos_x += 1
-            move(pos_x, pos_y, ambient)
-            action = True
+            if pos_x + 1 < ambient.shape[0]:
+                print("Moving down!")
+                clear_previous_move(pos_x, pos_y, ambient)
+                pos_x += 1
+                move(pos_x, pos_y, ambient)
+                action = True
         elif keyboard.is_pressed('right') and not action:
-            print("Moving right!")
-            clear_previous_move(pos_x, pos_y, ambient)
-            pos_y += 1
-            move(pos_x, pos_y, ambient)
-            action = True
+            if pos_y + 1 < ambient.shape[1]:
+                print("Moving right!")
+                clear_previous_move(pos_x, pos_y, ambient)
+                pos_y += 1
+                move(pos_x, pos_y, ambient)
+                action = True
         elif keyboard.is_pressed('left') and not action:
-            print("Moving left!")
-            clear_previous_move(pos_x, pos_y, ambient)
-            pos_y -= 1
-            move(pos_x, pos_y, ambient)
-            action = True
+            if pos_y - 1 >= 0:
+                print("Moving left!")
+                clear_previous_move(pos_x, pos_y, ambient)
+                pos_y -= 1
+                move(pos_x, pos_y, ambient)
+                action = True
         elif (not keyboard.is_pressed('up') and not keyboard.is_pressed('down')
               and not keyboard.is_pressed('left') and not keyboard.is_pressed('right')):
             action = False
